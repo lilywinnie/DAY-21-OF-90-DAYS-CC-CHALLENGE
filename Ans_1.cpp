e6252b5da541ef0231500d6d4e540e9337ad44f0
Shakira <287145569+lilywinnie@users.noreply.github.com>
2026-07-10 09:10:33 +0600

    Implement main function to process test cases

---
+++ Ans_1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int T;
+    cin>>T;
+    
+    while(T--){
+        
+        int N,X;
+        cin>>N>>X;
+        
+        cout << N/ (X*3) <<"\n";
+        
+    }
+    
+    return 0;
+}
